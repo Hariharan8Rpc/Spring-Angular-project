ac036039e6f2c776c6ce480c26606f64057b4b5a
Hariharan8Rpc <hh8rpc@gmail.com>
2022-12-24 19:59:41 +0530

    angular spring project

---
+++ .metadata/.plugins/org.eclipse.core.resources/.projects/Employee-backend/org.sonarlint.eclipse.core/issues/index.pb
@@ -0,0 +1,15 @@
+
+^
+.src/main/java/com/employee/model/Employee.java,4\b\4b1fd5da6ce77fa07ed58d9b66f515e0a650d3d0
+m
+=src/main/java/com/employee/controller/EmployeeController.java,3\5\35fb4d5b0bb7c882ee43eee08c7935b9162702f2
+s
+Csrc/main/java/com/employee/exception/ResourceNotFoundException.java,f\d\fdc8c387665464fd1de0cbbf8cd231674b8b41c8
+m
+=src/main/java/com/employee/repository/EmployeeRepository.java,c\d\cd3ff9fc43f505eff6c54621454507d46c5d1638
+Y
+)src/main/resources/application.properties,0\6\06f65155ec73600b9fc5c4dc2883d4bb84d8e284
+7
+pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
+j
+:src/main/java/com/employee/EmployeeBackendApplication.java,9\8\98b04306add5021b72704cc49ae5c30558ccaef0
---
+++ Employee-backend/src/main/java/com/employee/model/Employee.java
@@ -0,0 +1,58 @@
+package com.employee.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Table(name="employee")
+@Entity
+public class Employee {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private long id;
+	@Column(name="first_name")
+	private String firstName;
+	@Column(name="last_name")
+	private String lastName;
+	@Column(name="department")
+	private String department;
+	
+	
+	
+	public Employee() {
+		
+	}
+	public Employee(String firstName, String lastName, String department) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.department = department;
+	}
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getDepartment() {
+		return department;
+	}
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+}
