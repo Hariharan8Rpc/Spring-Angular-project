52a20ed344d1939368dfc8c744f3dc025b60c55c
Hariharan8Rpc <hh8rpc@gmail.com>
2022-12-25 21:45:37 +0530

    angular

--- Employee-backend/src/main/java/com/employee/controller/EmployeeController.java
+++ Employee-backend/src/main/java/com/employee/controller/EmployeeController.java
@@ -4,14 +4,17 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.employee.exception.ResourceNotFoundException;
 import com.employee.model.Employee;
 import com.employee.repository.EmployeeRepository;
 
@@ -42,5 +45,14 @@ public class EmployeeController {
 	@GetMapping("/find/{id}")
 	public Optional<Employee> findEmployee(@PathVariable("id") long id) {
 		return repo.findById(id);
+	}
+	@PutMapping("/update/{id}")
+	public ResponseEntity<Employee> update(@PathVariable Long id,@RequestBody Employee employee) {
+			Employee employee1=repo.findById(id).orElseThrow(()-> new ResourceNotFoundException("Employee Not Exist id:"+ id));
+			employee1.setFirstName(employee.getFirstName());
+			employee1.setLastName(employee.getLastName());
+			employee1.setDepartment(employee.getDepartment());			
+//			Employee updatedemp=repo.save(employee1);
+		return ResponseEntity.ok(repo.save(employee1));
 	}
 }
--- Employee-backend/src/main/java/com/employee/repository/EmployeeRepository.java
+++ Employee-backend/src/main/java/com/employee/repository/EmployeeRepository.java
@@ -10,7 +10,7 @@ import com.employee.model.Employee;
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee,Long>{
 	
-	@Query(value="select * from employee e where e.id like %:keyword% or e.last_name like %keyword% or e.first_name like %keyword% or e.department like %keyword%",nativeQuery=true)
+	@Query(value="select * from employee e where e.id like %:keyword% or e.last_name like %:keyword% or e.first_name like %:keyword% or e.department like %:keyword%",nativeQuery=true)
 	public List<Employee> findByKeyword(String keyword);
 
 }
